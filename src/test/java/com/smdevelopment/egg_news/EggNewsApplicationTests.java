5cd99c159d01608898bc6eeb9a6d15b8ec312d38
Santiago Moline <molinesanti@gmail.com>
2023-12-13 16:53:40 -0300

    First commit.

---
+++ src/test/java/com/smdevelopment/egg_news/EggNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smdevelopment.egg_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
